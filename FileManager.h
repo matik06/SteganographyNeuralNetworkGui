63e5a5705abdab52d15dce16212bb3bc4b6a8459
Mateusz Lubański <m.lubanskii@gmail.com>
2010-05-12 00:18:21 +0200

    napisana klasa file manager  - wczytywanie plików  - zapisywanie plików

--- FileManager.h
+++ FileManager.h
@@ -1,17 +1,54 @@
 /*
  * FileManager.h
  *
- *  Created on: 2010-05-04
+ *  Created on: 2010-05-07
  *      Author: matik
  */
 
 #ifndef FILEMANAGER_H_
 #define FILEMANAGER_H_
 
+#include <QDir>
+#include <QFile>
+#include <QTextStream>
+#include <QChar>
+
 class FileManager {
+
+protected:
+
+	/**
+	 * file for reading of gor saving data
+	 */
+	QFile * _file;
+	static QString _sMainFolderName;
+
 public:
+	FileManager(QString fileName);
+	FileManager(QString path, QString fileName);
 	FileManager();
+
 	virtual ~FileManager();
+
+
+
+	bool readData(int iNrInput, int iNrOutput, double **& aInputs, double **& aOutputs);
+
+	bool createFileWithWeights(int iNrLayer, int iNrNeurons [], int iNrWeights,
+			double *** aWeights);
+
+protected:
+
+	static void createMainFoder(QString folderName);
+
+	void readData(QTextStream & ts, double * aData, int iDataSize);
+	void readChar(QTextStream & ts, char c);
+	void initializeArray(double **& aData, int iNrDataSet, int iNrData);
+
+	void writeComa(QTextStream & ts, int index, int max);
+
+	int countNrSets(QTextStream & ts);
+
 };
 
 #endif /* FILEMANAGER_H_ */
